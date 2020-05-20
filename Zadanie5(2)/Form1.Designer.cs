a32a643c12e40d12a8d215d8afa43b3ae2c2ca69
Paweł Kowalski <paw.kowalski@protonmail.com>
2020-05-20 22:00:10 +0200

    added zadanie5(2)

--- Zadanie5(2)/Form1.Designer.cs
+++ Zadanie5(2)/Form1.Designer.cs
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Imie = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Nazwisko = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Wiek = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(526, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Zapisz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Imie,
+            this.Nazwisko,
+            this.Wiek});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(508, 429);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Imie
+            // 
+            this.Imie.Text = "Imie";
+            this.Imie.Width = 204;
+            // 
+            // Nazwisko
+            // 
+            this.Nazwisko.Text = "Nazwisko";
+            this.Nazwisko.Width = 207;
+            // 
+            // Wiek
+            // 
+            this.Wiek.Text = "Wiek";
+            this.Wiek.Width = 89;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(526, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(526, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(526, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(157, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(607, 119);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Załaduj";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(704, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Imie;
+        private System.Windows.Forms.ColumnHeader Nazwisko;
+        private System.Windows.Forms.ColumnHeader Wiek;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
---
+++ Zadanie5(2)/Osoba.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Zadanie5_2_
+{
+    [XmlRoot(ElementName = "Osoba")]
+    public class Osoba
+    {
+        [XmlElement(ElementName = "Imie")]
+        public string Imie;
+        [XmlElement(ElementName = "Nazwisko")]
+        public string Nazwisko;
+        [XmlElement(ElementName = "Wiek")]
+        public int Wiek;
+        
+        public Osoba(string imie, string nazwisko, int wiek)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Wiek = wiek;
+        }
+
+        public Osoba()
+        {
+            Imie = "";
+            Nazwisko = "";
+            Wiek = 0;
+        }
+
+        public string ToString()
+        {
+            return "Imie: " + Imie + " Nazwisko: " + Nazwisko + " Wiek: " + Wiek;
+        }
+
+        public bool Equals(Osoba osoba)
+        {
+            bool equals = false;
+            if (this.Imie == osoba.Imie && this.Nazwisko == osoba.Nazwisko && this.Wiek == osoba.Wiek)
+            {
+                equals = true;
+            }
+            return equals;
+        }
+    }
+
+}
