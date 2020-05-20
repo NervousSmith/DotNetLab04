55ec17c4bea4234790db5e7e9a46dd074025e741
Michał Durawa <michal.durawa@gmail.com>
2020-05-20 23:26:29 +0200

    added zad3

---
+++ Zadanie3/MyService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Zadanie3
+{
+    partial class MyService : ServiceBase
+    {
+        Timer timer;
+        public MyService()
+        {
+            InitializeComponent();
+            timer = new Timer(1000);
+            timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] line = new String[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\tmp\demo\file.txt", line);
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
--- Zadanie3/Program.cs
+++ Zadanie3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace Zadanie3
 {
@@ -10,6 +11,20 @@ namespace Zadanie3
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<MyService>(service =>
+               {
+                   service.ConstructUsing(s => new MyService());
+                   service.WhenStarted(s => s.Start());
+                   service.WhenStopped(s => s.Stop());
+               });
+
+                configure.RunAsLocalSystem();
+                configure.SetServiceName("MyService");
+                configure.SetDisplayName("Moja usługa");
+                configure.SetDescription("To jest prosta usługa");
+            });
         }
     }
 }
