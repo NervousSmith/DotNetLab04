71d01d9fc7dc88a5f74f111e8241c1c06a838997
Paweł Kowalski <paw.kowalski@protonmail.com>
2020-05-21 16:47:37 +0200

    added zadanie 6(3)

---
+++ Zadanie6(3)/Osoby.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Zadanie6_3_
+{
+    public class Osoby
+    {
+        [XmlElement("Osoba")]
+        public List<Osoba> osoby;
+
+        public Osoby()
+        {
+            osoby = new List<Osoba>();
+        }
+
+        public void Add(Osoba osobaDoDodania)
+        {
+            osoby.Add(osobaDoDodania);
+        }
+
+        public Osoba Get(int index)
+        {
+            return osoby[index];
+        }
+
+        public void Remove(Osoba osobaDoZabrania)
+        {
+            osoby.Remove(osobaDoZabrania);
+        }
+
+        public int Size()
+        {
+            return osoby.Count;
+        }
+    }
+}
--- Zadanie6(3)/Program.cs
+++ Zadanie6(3)/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace Zadanie6_3_
 {
@@ -10,6 +12,47 @@ namespace Zadanie6_3_
     {
         static void Main(string[] args)
         {
+            //UseDtd();
+            UseSchema();
+        }
+
+        private static void UseSchema()
+        {
+            Console.WriteLine("Walidacja pliku XML za pomocą XML Schema");
+            Console.ReadLine();
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas.Add("http://tempuri.org/XMLSchema1.xsd", "XMLSchema1.xsd");
+            ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+            XmlReader reader = XmlReader.Create("osoby.xml", settings);
+            XmlDocument document = new XmlDocument();
+            document.Load(reader);
+            document.Validate(eventHandler);
+        }
+
+        private static void UseDtd()
+        {
+            Console.WriteLine("Walidacja pliku XML za pomocą DTD");
+            Console.ReadLine();
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            settings.ValidationType = ValidationType.DTD;
+            ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+            XmlReader reader = XmlReader.Create("osoby.xml", settings);
+            while (reader.Read()) ;
+        }
+
+        private static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            switch (e.Severity)
+            {
+                case XmlSeverityType.Error:
+                    Console.WriteLine("Error: {0}", e.Message);
+                    break;
+                case XmlSeverityType.Warning:
+                    Console.WriteLine("Warning {0}", e.Message);
+                    break;
+            }
         }
     }
 }
